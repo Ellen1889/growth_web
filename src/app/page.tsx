71c5213151a49fa60d8b0a848f35d2a8b499680b
Ellen1889 <ellen.itopia@gmail.com>
2025-11-21 16:35:55 +0100

    Add auto-revalidation for Notion data updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { getExperiments, getReviews, getNewsletter, getTerms, getTools } from '@/lib/notion';
 import DashboardClient from '@/components/DashboardClient';
 
+// Revalidate data every 60 seconds
+export const revalidate = 60;
+
 export default async function Home() {
   const [experiments, reviews, newsletter, terms, tools] = await Promise.all([
     getExperiments(),
